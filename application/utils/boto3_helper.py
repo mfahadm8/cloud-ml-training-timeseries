df1215ba09cb448832bbd6787e0a5cc1e8b9adac
Ubuntu <ubuntu@ip-172-31-5-30.ec2.internal>
2024-08-07 09:42:57 +0000

    bug fix

--- application/utils/boto3_helper.py
+++ application/utils/boto3_helper.py
@@ -6,8 +6,10 @@ import smtplib
 from datetime import datetime
 import json
 
+AWS_DEFAULT_REGION = os.environ.get("AWS_DEFAULT_REGION","us-east-1")
+
 def download_from_s3(s3_uri, local_path):
-    s3 = boto3.client('s3')
+    s3 = boto3.client('s3',region_name=AWS_DEFAULT_REGION)
     bucket, key = s3_uri.replace("s3://", "").split("/", 1)
     
     if not key.endswith('/'):
@@ -29,7 +31,7 @@ def download_from_s3(s3_uri, local_path):
 
 
 def upload_script_to_s3(script_file,output_file, bucket_name, email, submission_timestamp):
-    s3 = boto3.client('s3')
+    s3 = boto3.client('s3',region_name=AWS_DEFAULT_REGION)
     accepted_script_key = f"unprocessed/accepted/{email}/{submission_timestamp}/{script_file}"
     accepted_output_key = f"unprocessed/accepted/{email}/{submission_timestamp}/{output_file}"
     s3.upload_file(script_file, bucket_name, accepted_script_key)
@@ -37,7 +39,7 @@ def upload_script_to_s3(script_file,output_file, bucket_name, email, submission_
 
 
 def update_submissions_dynamodb(email, submission_timestamp, script_file, output_file, retrain,sharpe_ratio):
-    dynamodb = boto3.client('dynamodb')
+    dynamodb = boto3.client('dynamodb',region_name=AWS_DEFAULT_REGION)
     current_date = datetime.now().strftime("%Y/%m")
     
     retrain_record = {
@@ -64,7 +66,7 @@ def update_submissions_dynamodb(email, submission_timestamp, script_file, output
     )
 
 def upload_weights_to_s3(bucket_name, email, submission_timestamp):
-    s3 = boto3.client('s3')
+    s3 = boto3.client('s3',region_name=AWS_DEFAULT_REGION)
     prefix = f"unprocessed/trainings/{email}/{submission_timestamp}"
     
     current_date = datetime.now().strftime("%Y/%m")
@@ -74,7 +76,7 @@ def upload_weights_to_s3(bucket_name, email, submission_timestamp):
     s3.upload_file('data/training_results.csv', bucket_name, output_key)
      
 def get_ssm_parameter(name):
-    ssm_client = boto3.client('ssm',region_name="us-east-1")
+    ssm_client = boto3.client('ssm',region_name=AWS_DEFAULT_REGION)
     response = ssm_client.get_parameter(
         Name=name,
         WithDecryption=True
@@ -103,7 +105,7 @@ def send_failure_email(email, message):
     server.quit()
 
 def store_sharpe_ratio_in_dynamodb(sharpe_ratio, submission_timestamp, email, user_name, model_name):
-    dynamodb = boto3.client('dynamodb')
+    dynamodb = boto3.client('dynamodb',region_name=AWS_DEFAULT_REGION)
     
     # Define the update expression and attribute values
     update_expression = "SET sharpe = :s, user_name = :un, model_name = :mn"
